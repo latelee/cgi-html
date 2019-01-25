b171588e1727a6886eb3baa877588205bdd80a57
Late Lee <li@latelee.org>
2019-01-25 13:10:39 +0800

    重构头部、导航、底部页面。
    Signed-off-by: Late Lee <li@latelee.org>

---
+++ html/head.js
@@ -0,0 +1,9 @@
+document.writeln("  <meta http-equiv=\'X-UA-Compatible\' content=\'IE=edge\'>");
+document.writeln("  <meta name=\'viewport\' content=\'width=device-width, initial-scale=1\'>");
+document.writeln("  <link href=\'img/logo.ico\' rel=\'shortcut icon\' type=\'image/x-icon\'/>");
+document.writeln("  <link rel=\'stylesheet\' href=\'css/main.css\'> ");
+document.writeln("  <link href=\'css/bootstrap.min.css\' rel=\'stylesheet\'> ");
+document.writeln("  <link rel=\'stylesheet\' href=\'css/font-awesome.min.css\'>");
+document.writeln("  <script src=\'js/jquery-2.0.0.min.js\'></script><script src=\'js/bootstrap.min.js\'></script>");
+document.writeln("  <script src=\'js/main.js\'></script>");
+document.writeln("  <title>叠加器管理后台</title>");
